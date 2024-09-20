91baaefe31d414a9f1902e97c3cd4b38faf09b42
fpereira <flavioteixeira2000@gmail.com>
2024-09-20 12:04:38 +0100

    multiplos 2,3,5

--- multiplos/main.c
+++ multiplos/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int v;
+
+
+    printf("Digite o valor:");
+    scanf("%d",&v);
+    if(v%2==0){
+      printf("%d e multiplo de 2\n",v);
+    }
+     if(v%3==0){
+        printf("%d e multiplo de 3\n",v);
+     }
+     if(v%5==0){
+        printf("%d e multiplo de 5\n",v);
+     }
+
     return 0;
 }
